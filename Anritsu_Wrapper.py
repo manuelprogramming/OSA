189128faa6e877d6ccc1384af90aa68c04bc7f29
Emanuel Pegler <manuel.pegler@gmail.com>
2021-10-15 13:39:13 +0200

    first working Version of the Anritsu_Wrapper added Setting.json

--- Anritsu_Wrapper.py
+++ Anritsu_Wrapper.py
@@ -1,8 +1,7 @@
 import pyvisa as visa
 import numpy as np
-import matplotlib.pyplot as plt
 from time import sleep
-import json
+
 
 
 class Anritsu_MS9740B:
@@ -124,29 +123,5 @@ class Anritsu_MS9740B:
         self.write(f"MPT {sampling_points}")
 
 
-def main():
-    with open('Settings.json') as json_file:
-        data = json.load(json_file)
 
-    sampling_points = data["sampling_points"]
-    start_wavelength = data["start_wavelength"]
-    stop_wavelength = data["stop_wavelength"]
-    visa_search_term = data["visa_search_term"]
-
-    anri = Anritsu_MS9740B(visa_search_term)
-    anri.set_start_stop_wavelength(start_wavelength, stop_wavelength)
-    anri.set_sampling_points(sampling_points)
-
-    anri.do_single_sweap()
-
-    wavelength, trace = anri.get_data()
-
-    plt.plot(wavelength, trace)
-    plt.ylabel("Intensity [dBm]")
-    plt.xlabel("Wavelength [nm]")
-    plt.show()
-
-
-if __name__ == '__main__':
-    main()
 
