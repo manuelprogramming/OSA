f868310427371666d79a3f578554fa72cfece6aa
Emanuel Pegler <manuel.pegler@gmail.com>
2021-10-15 13:27:13 +0200

    first working Version of the Anritsu_Wrapper

--- Anritsu_Wrapper.py
+++ Anritsu_Wrapper.py
@@ -1,21 +1,27 @@
 import pyvisa as visa
 import numpy as np
 import matplotlib.pyplot as plt
+from time import sleep
 
 
 class Anritsu_MS9740B:
     """
     Hardware wrapper for Anritsu_MS9740B Optical Spectrum Analyzer
-
+    IP Adress: 130.75.93.77
+       Subnetz Maske: 255.255.255.0
+       Host Name: OSA-UPCJ9GGPCMD
     Parameters:
         visa_search_term (str): The address that is passed to
             ``visa.ResourceManager().open_resource()``
     """
+
     def __init__(self, visa_search_term):
         rm = visa.ResourceManager()
         self.inst = rm.open_resource(visa_search_term)
 
-    def query(self, query_text: str) -> None:
+        print("Connected to ", self.identify())
+
+    def query(self, query_text: str):
         """
         Enter a query text for testing purpose
         Args:
@@ -24,21 +30,28 @@ class Anritsu_MS9740B:
         Returns:
 
         """
-        self.inst.query(query_text)
+        return self.inst.query(query_text)
 
+    def write(self, command_text: str) -> None:
+        """
+        For writing command
+        Args:
+            command_text:
+        """
+        self.inst.write(command_text)
 
     def identify(self):
         """
         Returns:
             str: The response from an ``*IDN?`` GPIB query.
         """
-        return self.inst.query('*IDN?')
+        return self.query('*IDN?')
 
     def clear_all_registers(self) -> None:
         """
         Clears all the common registers
         """
-        self.inst.query("*CLS")
+        self.query("*CLS")
 
     def set_start_stop_wavelength(self, start: float = 600, stop: float = 1800) -> None:
         """
@@ -50,10 +63,14 @@ class Anritsu_MS9740B:
         Returns: None
 
         """
-        self.inst.query(f"WSS {start},{stop}")
+        self.write(f"WSS {start},{stop}")
 
     def get_start_stop_wavelength(self) -> (float, float):
-        return self.inst.query("WSS?")
+        """
+        Returns:start and stop wavelength of sweap
+
+        """
+        return self.query("WSS?")
 
     def start_single_sweep_1(self) -> None:
         """
@@ -61,28 +78,73 @@ class Anritsu_MS9740B:
         Returns: None
 
         """
-        end = self.inst.query("*CLS ; SSI ; ESR2?")
-        while end == 0:
-            end = self.inst.query("ESR2?")
 
-        # self.inst.query("SVCSV")
+    def check_status(self) -> None:
+        """
+        when sweapo_completed != 0 then it breaks the while loop and more commands can be send
+        Returns: None
+        """
+        sweap_completed = 0
+        self.write("*CLS")
+        while sweap_completed == 0:
+            sweap_completed = int(self.query(":STAT:EVEN:COND?"))
+            print(f"Sweaping... {sweap_completed}")
+            sleep(1)
 
-    def start_single_sweep_2(self):
-        self.inst.query("SSI ; *WAI ;")
+    def do_single_sweap(self) -> None:
+        """
+        perfomsn a single sweap and prints message on completion
+        Returns: None
+        """
+        self.write(":INIT")
+        self.check_status()
+        print(f"Completed Sweap")
+
+    def get_data(self, memory_slot: str = "DMB?") -> (np.array, np.array):
+        """
+        gets the trace data and wavewlnegth of current measurement
+        Returns: wavelength and trace data from givcen Memory
+        """
+        trace = self.query(memory_slot)  # getting trace Data
+        trace = [float(x) for x in trace.split()]
+
+        sample_points = int(self.query("MPT?"))
+        start_wave = float(self.query("STA?"))
+        stop_wave = float(self.query("STO?"))
+        wave_length = np.linspace(start_wave, stop_wave, sample_points)
+
+        return wave_length, trace
+
+    def set_sampling_points(self, sampling_points) -> None:
+        """
+        for setting sampling points
+        Args:
+            sampling_points: Number of sampling points {51|101|251|501|1001|2001|5001|10001|20001|50001}
+        Returns: None
+        """
+        self.write(f"MPT {sampling_points}")
 
 
 def main():
-    """IP Adress: 130.75.93.77
-       Subnetz Maske: 255.255.255.0
-       Host Name: OSA-UPCJ9GGPCMD
-    """
-    visa_search_term = 'TCPIP0::130.75.93.77::inst0::INSTR' # Example Term
+    sampling_points = 501  # 51|101|251|501|1001|2001|5001|10001|20001|50001
+    start_wavelength = 1350
+    stop_wavelength = 1450
+    visa_search_term = 'TCPIP0::130.75.93.77::inst0::INSTR'
 
     anri = Anritsu_MS9740B(visa_search_term)
-    print(anri.identify())
+    anri.set_start_stop_wavelength(start_wavelength, stop_wavelength)
+    anri.set_sampling_points(sampling_points)
 
-    print(anri.get_start_stop_wavelength())
-    print(anri.start_single_sweep_1())
+    anri.do_single_sweap()
+
+    wavelength, trace = anri.get_data()
+
+    plt.plot(wavelength, trace)
+    plt.ylabel("Intensity [dBm]")
+    plt.xlabel("Wavelenght [nm]")
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
+
