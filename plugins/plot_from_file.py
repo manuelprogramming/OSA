d94405ed542758bddb43dab245ea46bb39cdf901
profm <manuel.pegler@gmail.com>
2021-11-16 19:07:13 +0100

    plots points data and trace data now differently

--- plugins/plot_from_file.py
+++ plugins/plot_from_file.py
@@ -17,6 +17,7 @@ class PlotFromFile:
     result: BaseResult
 
     def do_work(self) -> BaseResult:
+        plt.style.use("seaborn-whitegrid")
         file_path = get_latest_file_path()
         if not file_path:
             self.result.msg = "no files in directory"
@@ -28,15 +29,34 @@ class PlotFromFile:
         self.result.msg = "plotted from file successful"
         return self.result
 
-    @staticmethod
-    def _plot_from_file(file_path) -> None:
-        plt.style.use("seaborn-whitegrid")
-        df = pd.read_csv(file_path, index_col=0)
-        df.plot()
+    def _plot_from_file(self, file_path) -> None:
+        if self._is_points_data(file_path):
+            self._plot_points_data(file_path)
+        else:
+            self._plot_trace_data(file_path)
+
         plt.ylabel("Intensity [dBm]")
         plt.tight_layout()
+        plt.legend()
         plt.show()
 
+    @staticmethod
+    def _is_points_data(file_path) -> bool:
+        df = pd.read_csv(file_path, index_col=0)
+        return df.index.name == "bending_radius"
+
+    def _plot_points_data(self, file_path):
+        df = pd.read_csv(file_path, index_col=[0, 1])
+        bending_radii = set([bending_radius for bending_radius in df.index.get_level_values('bending_radius')])
+        for bending_radius in bending_radii:
+            df_part = df.loc[bending_radius]
+            y = df_part["trace [dBm]"].to_numpy()
+            plt.plot(df_part.index.to_numpy(), y, "+", label=bending_radius)
+        plt.xlabel("Wavelength [nm]")
+
+    def _plot_trace_data(self, file_path):
+        df = pd.read_csv(file_path, index_col=0)
+        df.plot()
 
 def initialize() -> None:
     factory.register("plot_from_file", PlotFromFile)
