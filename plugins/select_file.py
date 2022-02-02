dbc9fb244f23535ae0a6376faeccd6b96d30e947
manuelprogramming <manuel.pegler@gmail.com>
2022-02-02 21:19:02 +0100

    fixed a bug where you could open any file type

--- plugins/select_file.py
+++ plugins/select_file.py
@@ -1,9 +1,8 @@
 from dataclasses import dataclass
-
+from typing import Tuple
 
 from tkinter.filedialog import askopenfilename
 from tkinter import Tk
-
 
 from osa import factory
 from handlers.result import BaseResult
@@ -19,8 +18,17 @@ class SelectFile:
     result: BaseResult
 
     def do_work(self) -> BaseResult:
-        self.showDialog()
+        file_name = self.showDialog()
+        self.result.value = file_name
+
+        valid_file_types = (".csv", ".xlsx", ".xls", ".dat", ".DAT", ".txt")
+        if not self.result.value.endswith(valid_file_types):
+            self._fail_result(valid_file_types)
+            return self.result
+
+        set_setting("selected_file", file_name)
         self.result.msg = f"Selected the file {self.result.value}"
+
         return self.result
 
     def showDialog(self):
@@ -33,12 +41,13 @@ class SelectFile:
         saving_path = get_saving_path()
         filename = askopenfilename(filetypes=file_type_filter, initialdir=saving_path)
 
-        set_setting("selected_file", filename)
         root.destroy()
 
-        self.result.value = filename
+        return filename
+
+    def _fail_result(self, valid_file_types: Tuple[str, ...]):
+        self.result.msg = f"The file chosen has not a valid type. Valid Types are {valid_file_types}"
 
 
 def initialize() -> None:
     factory.register("select_file", SelectFile)
-
